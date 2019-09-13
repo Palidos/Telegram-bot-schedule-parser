be87e7c67a37d90f85a4a48da412809b1996b6c4
Daniil Bederdinov <dbederdinov@ARTGROUP.local>
2019-09-13 18:37:33 +0300

    Change config of horoku from web to worker

--- index.js
+++ index.js
@@ -50,7 +50,3 @@ getScheduleInfo().then(info => {
 
 bot.launch();
 
-// let port = process.env.PORT || 3000;
-// app.listen(port, "0.0.0.0", function() {
-//   console.log("Listening on Port 3000");
-// });
